058a5295b42ed84f1ca0f276b4c615da85bceabb
sanchezespinosa1998 <sanchezespinosa1998@gmail.com>
2024-12-30 09:06:37 +0100

    update4

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+
+// Configuración básica
+export default defineConfig({
+  // Configuración específica para Vite
+
+  base: 'https://sanchezespinosa1998.github.io/portfolio_2', // Base pública para las rutas
+});
